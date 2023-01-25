7eae0c9a8575f303f1b404c95f6f160a933245cf
alon.itzhaky <alon.itzhaky@gmail.com>
2023-01-25 20:03:12 +0200

    Fixes to Logo and Favicon

--- code/frontend/app/src/App.tsx
+++ code/frontend/app/src/App.tsx
@@ -9,6 +9,7 @@ function App() {
       <Header/>
       <br/>
       <PhotoCarousel/>
+      <Product/>
     </div>
   );
 }
--- code/frontend/app/src/components/header/Header.tsx
+++ code/frontend/app/src/components/header/Header.tsx
@@ -1,53 +1,49 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub, faInstagram } from '@fortawesome/free-brands-svg-icons'
-import { faPhone } from '@fortawesome/free-solid-svg-icons'
+import { Container, Image, Navbar, Nav, NavbarBrand, NavItem, NavLink } from 'react-bootstrap';
+import { faGithub, faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
   return (
     <div>
-  <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-    <div className="container-fluid">
-      <div>
-      <a className="navbar-brand" href="#">
-        <img src="./logo.png" alt="Bootstrap" width="100" height="100" className="d-inline-block align-text-top" />
-      </a>
-      </div>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01"
-        aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarColor01">
-        <ul className="navbar-nav me-auto">
-          <li className="nav-item">
-            <a className="nav-link active" href="#">Home
-              <span className="visually-hidden">(current)</span>
+      <Navbar bg="dark" expand="lg" variant="dark" style={{backgroundColor: "#3f3a36"}}>
+        <Container fluid>
+          <NavbarBrand href="#">
+            <Image src="./logo.png" alt="Bootstrap" width="100" height="100" className="d-inline-block align-text-top" />
+          </NavbarBrand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+          <Navbar.Collapse id="navbarColor01">
+            <Nav className="me-auto" navbar>
+              <NavItem>
+                <NavLink href="#" active>Home</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#">About Us</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#">Shop</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#">Contact</NavLink>
+              </NavItem>
+            </Nav>
+            <Navbar.Text>
+              <a href="https://github.com/alonitzhaking" target="_blank" style={{ color: "white" }}>
+                <FontAwesomeIcon icon={faGithub} style={{ margin: "0 10px" }} />
+              </a>
+              <a href="https://www.instagram.com/alon.itzhaking" target="_blank" style={{ color: "white" }}>
+                <FontAwesomeIcon icon={faInstagram} style={{ margin: "0 10px" }} />
+              </a>
+              <a href="tel:+972547144714" target="_blank" style={{ color: "white" }}>
+              <FontAwesomeIcon icon={faPhone} style={{ margin: "0 10px" }} />
             </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">About Us</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Shop</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Contact</a>
-          </li>
-        </ul>
-        <a href="https://github.com/alonitzhaky" target="_blank" style={{color: "white"}}>
-          <FontAwesomeIcon icon={faGithub} style={{margin: "0 10px"}}  />
-        </a>
-        <a href="https://www.instagram.com/alon.itzhaky" target="_blank" style={{color: "white"}}>
-          <FontAwesomeIcon icon={faInstagram} style={{margin: "0 10px"}}/>
-          </a>
-          <a href="tel:+972547144714" target="_blank" style={{color: "white"}}>
-          <FontAwesomeIcon icon={faPhone} style={{margin: "0 10px"}}/>
-          </a>
-      </div>
-    </div>
-  </nav>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   )
 }
 
-export default Header;
+export default Header 
