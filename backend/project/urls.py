2498eaceec705617ae676f7dc2d07a3f489eea93
alon.itzhaky <alon.itzhaky@gmail.com>
2023-02-17 22:35:34 +0200

    Changes to urls.py

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.views.static import serve
+from django.conf.urls.static import static
 # from . import views 
 
 urlpatterns = [
@@ -25,5 +25,5 @@ urlpatterns = [
 ]
 
 # Connect MEDIA 
-urlpatterns += serve(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += serve(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
