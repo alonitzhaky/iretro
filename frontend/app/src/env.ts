250006aab0360c6de819333047d5ca923fefc8d0
alon.itzhaky <alon.itzhaky@gmail.com>
2023-02-21 01:05:28 +0200

    Changes to env.ts

--- frontend/app/src/env.ts
+++ frontend/app/src/env.ts
@@ -1,7 +1,7 @@
-export const SERVER = "http://0.0.0.0:8000"
+// export const SERVER = "http://0.0.0.0:8000"
 
-// export const SERVER = "https://iretro.onrender.com" 
+export const SERVER = "https://iretro.onrender.com" 
 
-// If you wish to compose using Docker, please un-comment line 3 and comment line no. 1. 
+// If you wish to compose using Docker, please disable line 3 and comment line no. 1. 
 
 export let webColor = "rgb(128, 95, 75)"
